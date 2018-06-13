18511d86d0600f70a2be92d6a6f2e977067949b7
Nils Bussas <nils.bussas@gmail.com>
2018-06-13 19:44:53 +0200

    merge testutils read and write files

--- testutils/io.go
+++ testutils/io.go
@@ -2,6 +2,32 @@ package testutils
 
 import "github.com/nilsbu/lastfm/io"
 
+// strerr is a simple error that consists of a string.
+type strerr string
+
+func (s strerr) Error() string {
+	return string(s)
+}
+
+// Reader is a mock reader that stores data that is returned on request.
+// For each request of the same resource the same data is returned.
+type Reader struct {
+	data map[io.Resource][]byte
+}
+
+// NewReader constructs a reader.
+func NewReader(data map[io.Resource][]byte) *Reader {
+	return &Reader{data}
+}
+
+func (r *Reader) Read(rsrc *io.Resource) (data []byte, err error) {
+	data, ok := r.data[*rsrc]
+	if ok {
+		return data, nil
+	}
+	return nil, strerr("resource not found")
+}
+
 // Writer is a mock writer that stores data that is written.
 // When a resource is written more than all instances are stored.
 // Certain resources can throw errors. failSequences stores on which write
