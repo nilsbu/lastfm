b2adbe19772b2cff4bade772cd14f0b8208a212b
Nils Bussas <nils.bussas@gmail.com>
2018-06-16 15:57:04 +0200

    LoadAllDayPlays

--- organize/recenttracks.go
+++ organize/recenttracks.go
@@ -7,58 +7,78 @@ import (
 	"github.com/nilsbu/lastfm/unpack"
 )
 
-// LoadDayPlaysResult is the result of LoadDayPlays.
+// LoadAllDayPlays load plays from all days since the registration of the user.
+// TODO consistant naming scheme between read, write, download, load, get etc.
+func LoadAllDayPlays(
+	user unpack.User,
+	until io.Midnight,
+	r io.AsyncReader) ([]unpack.DayPlays, error) {
+
+	days := int((until - user.Registered) / 86400)
+	result := make([]unpack.DayPlays, days+1)
+	feedback := make(chan error)
+	for i := range result {
+		go func(i int) {
+			date := io.Midnight(i)*86400 + user.Registered
+			dp, err := loadDayPlays(user.Name, date, r)
+			if err == nil {
+				result[i] = dp
+			}
+			feedback <- err
+		}(i)
+	}
+	fail := []error{}
+	for i := 0; i <= days; i++ {
+		err := <-feedback
+		if err != nil {
+			fail = append(fail, err)
+		}
+	}
+	if len(fail) > 0 {
+		return nil, fail[0] // TODO return all errors
+	}
+	return result, nil
+}
+
+// LoadDayPlaysResult is the result of loadDayPlays.
 type LoadDayPlaysResult struct {
 	DayPlays unpack.DayPlays
 	Err      error
 }
 
-// LoadDayPlays load plays from one day.
-// TODO consistant naming scheme between read, write, download, load, get etc.
-func LoadDayPlays(
+func loadDayPlays(
 	user io.Name,
 	time io.Midnight,
-	r io.AsyncReader) <-chan LoadDayPlaysResult {
+	r io.AsyncReader) (unpack.DayPlays, error) {
+	dp, pages, err := loadDayPlaysPage(io.NewUserRecentTracks(user, 1, time), r)
+	if err != nil || pages == 1 {
+		return dp, err
+	}
 
-	out := make(chan LoadDayPlaysResult)
-	go func() {
+	back := make(chan LoadDayPlaysResult)
+	for p := 1; p < pages; p++ {
+		go func(p io.Page) {
 
-		dp, pages, err := loadDayPlaysPage(io.NewUserRecentTracks(user, 1, time), r)
-		if err != nil || pages == 1 {
-			out <- LoadDayPlaysResult{dp, err}
-			close(out)
-			return
+			tmpDP, _, tmpErr := loadDayPlaysPage(
+				io.NewUserRecentTracks(user, p+1, time), r)
+
+			back <- LoadDayPlaysResult{tmpDP, tmpErr}
+		}(io.Page(p))
+	}
+
+	for p := 1; p < pages; p++ {
+		dpr := <-back
+		if dpr.Err != nil {
+			return nil, dpr.Err
 		}
 
-		back := make(chan LoadDayPlaysResult)
-		for p := 1; p < pages; p++ {
-			go func(p io.Page) {
-
-				tmpDP, _, tmpErr := loadDayPlaysPage(
-					io.NewUserRecentTracks(user, p+1, time), r)
-
-				back <- LoadDayPlaysResult{tmpDP, tmpErr}
-			}(io.Page(p))
+		for k, v := range dpr.DayPlays {
+			dp[k] += v
 		}
+	}
+	close(back)
 
-		for p := 1; p < pages; p++ {
-			dpr := <-back
-			if dpr.Err != nil {
-				out <- LoadDayPlaysResult{nil, dpr.Err}
-				close(out)
-				return
-			}
-
-			for k, v := range dpr.DayPlays {
-				dp[k] += v
-			}
-		}
-		close(back)
-
-		out <- LoadDayPlaysResult{dp, err}
-		close(out)
-	}()
-	return out
+	return dp, err
 }
 
 func loadDayPlaysPage(rsrc *io.Resource,
--- organize/recenttracks_test.go
+++ organize/recenttracks_test.go
@@ -10,6 +10,68 @@ import (
 	"github.com/nilsbu/lastfm/unpack"
 )
 
+func TestLoadAllDayPlays(t *testing.T) {
+	ft := fastest.T{T: t}
+
+	testCases := []struct {
+		user  unpack.User
+		until io.Midnight
+		data  [][]string
+		dps   []unpack.DayPlays
+		err   fastest.Code
+	}{
+		{
+			unpack.User{Name: "", Registered: 0},
+			86400,
+			[][]string{[]string{}, []string{}},
+			nil,
+			fastest.Fail,
+		},
+		{
+			unpack.User{Name: "ASDF", Registered: 86400},
+			2 * 86400,
+			[][]string{
+				[]string{`{"recenttracks":{"track":[{"artist":{"#text":"ASDF"}}], "@attr":{"totalPages":"1"}}}`},
+				[]string{`{"recenttracks":{"track":[{"artist":{"#text":"XXX"}}], "@attr":{"totalPages":"1"}}}`},
+			},
+			[]unpack.DayPlays{unpack.DayPlays{"ASDF": 1}, unpack.DayPlays{"XXX": 1}},
+			fastest.OK,
+		},
+		{
+			unpack.User{Name: "ASDF", Registered: 0},
+			0,
+			[][]string{
+				[]string{
+					`{"recenttracks":{"track":[{"artist":{"#text":"X"}}], "@attr":{"page":"1","totalPages":"3"}}}`,
+					`{"recenttracks":{"track":[{"artist":{"#text":"Y"}}], "@attr":{"page":"2","totalPages":"3"}}}`,
+					`{"recenttracks":{"track":[{"artist":{"#text":"Z"}}, {"artist":{"#text":"X"}}], "@attr":{"page":"3","totalPages":"3"}}}`,
+				},
+			},
+			[]unpack.DayPlays{unpack.DayPlays{"X": 2, "Y": 1, "Z": 1}},
+			fastest.OK,
+		},
+	}
+
+	for i, tc := range testCases {
+		ft.Seq(fmt.Sprintf("#%v", i), func(ft fastest.T) {
+			files := make(map[io.Resource][]byte)
+			for j, day := range tc.data {
+				for k, d := range day {
+					time := tc.user.Registered + io.Midnight(j*86400)
+					urt := io.NewUserRecentTracks(tc.user.Name, io.Page(k+1), time)
+					files[*urt] = []byte(d)
+				}
+			}
+			r := testutils.AsyncReader(files)
+
+			dps, err := LoadAllDayPlays(tc.user, tc.until, r)
+			ft.Implies(err != nil, tc.err == fastest.Fail, err)
+			ft.Implies(err == nil, tc.err == fastest.OK)
+			ft.DeepEquals(dps, tc.dps)
+		})
+	}
+}
+
 func TestLoadDayPlays(t *testing.T) {
 	ft := fastest.T{T: t}
 
@@ -81,12 +143,12 @@ func TestLoadDayPlays(t *testing.T) {
 			}
 			r := testutils.AsyncReader(files)
 
-			dpRes := <-LoadDayPlays(tc.user, tc.time, r)
-			ft.Implies(dpRes.Err != nil, tc.err == fastest.Fail, dpRes.Err)
-			ft.Implies(dpRes.Err == nil, tc.err == fastest.OK)
+			dp, err := loadDayPlays(tc.user, tc.time, r)
+			ft.Implies(err != nil, tc.err == fastest.Fail, err)
+			ft.Implies(err == nil, tc.err == fastest.OK)
 
-			ft.Implies(dpRes.DayPlays == nil, tc.dp == nil)
-			ft.DeepEquals(dpRes.DayPlays, tc.dp)
+			ft.Implies(dp == nil, tc.dp == nil)
+			ft.DeepEquals(dp, tc.dp)
 		})
 	}
 }
