722a79383378da7def5e0abc5f7eafc920dbc7a3
Nils Bussas <nils.bussas@gmail.com>
2018-06-23 00:01:00 +0200

    refactor channels tests

--- pkg/io/channels_test.go
+++ pkg/io/channels_test.go
@@ -2,72 +2,73 @@ package io
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 
-	"github.com/nilsbu/fastest"
 	"github.com/nilsbu/lastfm/pkg/rsrc"
 )
 
 func TestSeqReaderRead(t *testing.T) {
-	ft := fastest.T{T: t}
 	userInfo, _ := rsrc.UserInfo("SOX")
 
-	testCases := []struct {
+	cases := []struct {
 		rs   rsrc.Resource
 		data string
-		err  fastest.Code
+		ok   bool
 	}{
-		{rsrc.APIKey(), "XX", fastest.OK},
-		{userInfo, "", fastest.OK},
-		{userInfo, "lol", fastest.Fail},
+		{rsrc.APIKey(), "XX", true},
+		{userInfo, "", true},
+		{userInfo, "lol", false},
 	}
 
-	for i, tc := range testCases {
-		ft.Seq(fmt.Sprintf("#%v", i), func(ft fastest.T) {
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
 			r := make(SeqReader)
 
 			go func() {
 				for job := range r {
-					path1, err1 := job.Resource.Path()
-					path2, err2 := tc.rs.Path()
-					ft.Nil(err1)
-					ft.Nil(err2)
+					path1, _ := job.Resource.Path()
+					path2, _ := c.rs.Path()
 
-					if path1 == path2 && tc.err == fastest.OK {
-						job.Back <- ReadResult{[]byte(tc.data), nil}
+					if path1 == path2 && c.ok == true {
+						job.Back <- ReadResult{[]byte(c.data), nil}
 					} else {
 						job.Back <- ReadResult{nil, errors.New("read failed")}
 					}
 				}
 			}()
 
-			data, err := r.Read(tc.rs)
+			data, err := r.Read(c.rs)
 
-			ft.Implies(err != nil, tc.err == fastest.Fail)
-			ft.Implies(err == nil, tc.err == fastest.OK, err)
-			ft.Only(err == nil)
-			ft.Equals(string(data), tc.data)
+			if err != nil && c.ok {
+				t.Error("unexpected error:", err)
+			} else if err == nil && !c.ok {
+				t.Error("expected error but non occurred")
+			}
+			if err == nil {
+				if string(data) != c.data {
+					t.Errorf("read data is wrong:\nread:     '%v'\nexpected: '%v'",
+						string(data), c.data)
+				}
+			}
 		})
 	}
 }
 
 func TestSeqWriterWrite(t *testing.T) {
-	ft := fastest.T{T: t}
 	userInfo, _ := rsrc.UserInfo("SOX")
 
-	testCases := []struct {
+	cases := []struct {
 		rs   rsrc.Resource
 		data string
-		err  fastest.Code
+		ok   bool
 	}{
-		{rsrc.APIKey(), "XX", fastest.OK},
-		{userInfo, "", fastest.OK},
-		{userInfo, "lol", fastest.Fail},
+		{rsrc.APIKey(), "XX", true},
+		{userInfo, "", true},
+		{userInfo, "lol", false},
 	}
 
-	for i, tc := range testCases {
-		ft.Seq(fmt.Sprintf("#%v", i), func(ft fastest.T) {
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
 			w := make(SeqWriter)
 
 			var data []byte
@@ -76,7 +77,7 @@ func TestSeqWriterWrite(t *testing.T) {
 				for job := range w {
 					data = job.Data
 					rs = job.Resource
-					if tc.err == fastest.OK {
+					if c.ok {
 						job.Back <- nil
 					} else {
 						job.Back <- errors.New("read failed")
@@ -84,18 +85,25 @@ func TestSeqWriterWrite(t *testing.T) {
 				}
 			}()
 
-			err := w.Write([]byte(tc.data), tc.rs)
-			ft.Implies(err != nil, tc.err == fastest.Fail)
-			ft.Implies(err == nil, tc.err == fastest.OK, err)
-			ft.Only(err == nil)
+			err := w.Write([]byte(c.data), c.rs)
+			if err != nil && c.ok {
+				t.Error("unexpected error:", err)
+			} else if err == nil && !c.ok {
+				t.Error("expected error but non occurred")
+			}
+			if err == nil {
+				path1, _ := rs.Path()
+				path2, _ := c.rs.Path()
 
-			path1, err1 := rs.Path()
-			path2, err2 := tc.rs.Path()
-			ft.Nil(err1)
-			ft.Nil(err2)
-			ft.Equals(path1, path2)
-			ft.Only(err == nil)
-			ft.Equals(string(data), tc.data)
+				if path1 != path2 {
+					t.Errorf("written to wrong path:\nwritten:  '%v'\nexpected: '%v'",
+						path1, path2)
+				}
+				if string(data) != c.data {
+					t.Errorf("written data is wrong:\nread:     '%v'\nexpected: '%v'",
+						string(data), c.data)
+				}
+			}
 		})
 	}
 }
