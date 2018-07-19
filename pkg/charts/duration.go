23d5e8e4c674127ef9790d27bab7006d1f024a3a
Nils Bussas <nils.bussas@gmail.com>
2018-07-19 21:43:47 +0200

    Intervals

--- pkg/charts/duration.go
+++ pkg/charts/duration.go
@@ -8,76 +8,116 @@ import (
 	"github.com/nilsbu/lastfm/pkg/rsrc"
 )
 
-type Interval interface {
-	Begin() int64
-	Before() int64
+// Interval is a time span. Begin is the first moment, Before is the moment
+// immediately after the interval ends. By convention both have to be at
+// midnight UTC so the interval represents full days only.
+type Interval struct {
+	Begin  time.Time
+	Before time.Time
 }
 
-type period struct {
-	begin  time.Time
-	before time.Time
+type Step int
+
+const (
+	Day Step = iota
+	Month
+	Year
+)
+
+type intervalIterator struct {
+	step     Step
+	interval Interval
+	before   int64
 }
 
-func (p period) Begin() int64 {
-	return p.begin.Unix()
-}
+func newIntervalIterator(step Step, from time.Time, before int64) *intervalIterator {
+	var interval Interval
 
-func (p period) Before() int64 {
-	return p.before.Unix()
-}
-
-func Period(descr string) (Interval, error) {
-	switch len(descr) {
-	case 4:
-		return year(descr)
-	case 7:
-		return month(descr)
+	switch step {
+	case Day:
+		interval = dayPeriod(from)
+	case Month:
+		interval = monthPeriod(from)
 	default:
-		return nil, fmt.Errorf("interval format '%v' not supported", descr)
+		interval = yearPeriod(from)
+	}
+
+	return &intervalIterator{
+		step:     step,
+		interval: interval,
+		before:   before,
 	}
 }
 
-func year(descr string) (Interval, error) {
-	begin, err := time.Parse("2006", descr)
-	if err != nil {
-		return nil, err
-	}
-
-	return yearPeriod(begin), nil
+func (ii *intervalIterator) HasNext() bool {
+	return ii.interval.Begin.Unix() < ii.before
 }
 
-func month(descr string) (Interval, error) {
-	begin, err := time.Parse("2006-01", descr)
-	if err != nil {
-		return nil, err
+func (ii *intervalIterator) Next() Interval {
+	interval := ii.interval
+
+	before := ii.interval.Before
+	switch ii.step {
+	case Day:
+		ii.interval = dayPeriod(before)
+	case Month:
+		ii.interval = monthPeriod(before)
+	case Year:
+		ii.interval = yearPeriod(before)
+	default:
 	}
-	return monthPeriod(begin), nil
+
+	return interval
 }
 
-func yearPeriod(t time.Time) Interval {
-	y := t.Year()
-	return &period{
-		begin:  time.Date(y, time.January, 1, 0, 0, 0, 0, time.UTC),
-		before: time.Date(y+1, time.January, 1, 0, 0, 0, 0, time.UTC),
+func dayPeriod(t time.Time) Interval {
+	y, m, d := t.Year(), t.Month(), t.Day()
+	return Interval{
+		Begin:  time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.UTC),
+		Before: time.Date(y, time.Month(m), d+1, 0, 0, 0, 0, time.UTC),
 	}
 }
 
 func monthPeriod(t time.Time) Interval {
 	y, m := t.Year(), t.Month()
-	return &period{
-		begin:  time.Date(y, time.Month(m), 1, 0, 0, 0, 0, time.UTC),
-		before: time.Date(y, time.Month(m+1), 1, 0, 0, 0, 0, time.UTC),
+	return Interval{
+		Begin:  time.Date(y, time.Month(m), 1, 0, 0, 0, 0, time.UTC),
+		Before: time.Date(y, time.Month(m+1), 1, 0, 0, 0, 0, time.UTC),
+	}
+}
+
+func yearPeriod(t time.Time) Interval {
+	y := t.Year()
+	return Interval{
+		Begin:  time.Date(y, time.January, 1, 0, 0, 0, 0, time.UTC),
+		Before: time.Date(y+1, time.January, 1, 0, 0, 0, 0, time.UTC),
+	}
+}
+
+func Period(descr string) (Interval, error) {
+	switch len(descr) {
+	case 4:
+		begin, err := time.Parse("2006", descr)
+		if err != nil {
+			return Interval{}, err
+		}
+		return yearPeriod(begin), nil
+	case 7:
+		begin, err := time.Parse("2006-01", descr)
+		if err != nil {
+			return Interval{}, err
+		}
+		return monthPeriod(begin), nil
+	default:
+		return Interval{}, fmt.Errorf("interval format '%v' not supported", descr)
 	}
 }
 
 func (c Charts) Interval(i Interval, registered rsrc.Day) Column {
 	size := c.Len()
 
-	offset, _ := registered.Midnight()
-	offset /= 86400
-	from := int(i.Begin()/86400 - offset - 1)
-
-	to := int(i.Before()/86400 - offset - 1)
+	from := index(i.Begin, registered)
+	to := index(i.Before, registered)
 	if to < 0 {
 		return Column{}
 	} else if to >= size {
@@ -99,4 +139,38 @@ func (c Charts) Interval(i Interval, registered rsrc.Day) Column {
 	}
 	sort.Sort(column)
 	return column
+}
+
+func index(t time.Time, registered rsrc.Day) int {
+	offset, _ := registered.Midnight()
+	return int((t.Unix()-offset)/86400 - 1)
+}
+
+func (c Charts) Intervals(step Step, registered rsrc.Day) Charts {
+	reg, _ := registered.Midnight()
+	ii := newIntervalIterator(
+		step,
+		time.Unix(reg, 0).UTC(),
+		reg+int64(86400*c.Len()))
+
+	columns := []Column{}
+
+	for ii.HasNext() {
+		current := ii.Next()
+
+		columns = append(columns, c.Interval(current, registered))
+	}
+
+	result := Charts{}
+	for _, val := range columns[0] {
+		result[val.Name] = make([]float64, len(columns))
+	}
+
+	for m, column := range columns {
+		for _, val := range column {
+			result[val.Name][m] = val.Score
+		}
+	}
+
+	return result
 }
--- pkg/charts/duration_test.go
+++ pkg/charts/duration_test.go
@@ -101,3 +101,62 @@ func TestChartsPeriod(t *testing.T) {
 		})
 	}
 }
+
+func iotaF(n int) []float64 {
+	nums := make([]float64, n)
+	for i := range nums {
+		nums[i] = float64(i)
+	}
+
+	return nums
+}
+
+func repeat(x, n int) []float64 {
+	nums := make([]float64, n)
+	for i := range nums {
+		nums[i] = float64(x)
+	}
+
+	return nums
+}
+
+func TestChartsSumIntervals(t *testing.T) {
+	cases := []struct {
+		charts     Charts
+		step       Step
+		registered rsrc.Day
+		intervals  Charts
+	}{
+		{
+			Charts{},
+			Month, rsrc.ParseDay("2011-10-11"),
+			Charts{},
+		},
+		{
+			Charts{"a": []float64{12, 33, 10}},
+			Day, rsrc.ParseDay("2011-10-11"),
+			Charts{"a": []float64{12, 33, 10}},
+		},
+		{
+			Charts{"a": iotaF(30), "b": repeat(1, 30)},
+			Month, rsrc.ParseDay("2011-10-11"),
+			Charts{"a": []float64{210, 225}, "b": []float64{21, 9}},
+		},
+		{
+			Charts{"a": repeat(2, 400)},
+			Year, rsrc.ParseDay("2011-12-25"),
+			Charts{"a": []float64{14, 732, 54}},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			sum := c.charts.Sum()
+			intervals := sum.Intervals(c.step, c.registered)
+
+			if !reflect.DeepEqual(intervals, c.intervals) {
+				t.Errorf("wrong data:\nhas:  %v\nwant: %v", intervals, c.intervals)
+			}
+		})
+	}
+}
