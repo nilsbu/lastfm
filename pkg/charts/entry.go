d4616a0d088c356fe4637e10bb473b18708ced4e
Nils Bussas <nils.bussas@gmail.com>
2018-11-04 21:02:59 +0100

    chart entry funcs

---
+++ pkg/charts/entry.go
@@ -0,0 +1,73 @@
+package charts
+
+import (
+	"time"
+
+	"github.com/nilsbu/lastfm/pkg/rsrc"
+)
+
+type EntryDate struct {
+	Name string
+	Date rsrc.Day
+}
+
+func (c Charts) FindEntryDates(registered rsrc.Day, threshold float64,
+) (entryDates []EntryDate) {
+
+	for name, values := range c {
+		if values[len(values)-1] < threshold {
+			continue
+		}
+
+		for i, value := range values {
+			if value >= threshold {
+				date, _ := registered.Midnight()
+				date += int64(86400 * i)
+				entryDates = append(entryDates, EntryDate{name, rsrc.ToDay(date)})
+				break
+			}
+		}
+	}
+
+	return
+}
+
+func FilterEntryDates(entryDates []EntryDate, cutoff rsrc.Day,
+) (filtered []EntryDate) {
+	cutoffM, _ := cutoff.Midnight()
+	for _, entryDate := range entryDates {
+		date, _ := entryDate.Date.Midnight()
+		if date >= cutoffM {
+			filtered = append(filtered, entryDate)
+		}
+	}
+
+	return
+}
+
+func (c Charts) GetYearPartition(registered rsrc.Day, threshold float64,
+) Partition {
+	entryDates := c.FindEntryDates(registered, threshold)
+
+	p := mapPart{
+		assoc:      make(map[string]string),
+		partitions: []string{},
+	}
+
+	for _, entryDate := range entryDates {
+		m, _ := entryDate.Date.Midnight()
+		p.assoc[entryDate.Name] = time.Unix(m, 0).UTC().Format("2006")
+	}
+
+	reg, _ := registered.Midnight()
+	ii := newIntervalIterator(
+		Year,
+		time.Unix(reg, 0).UTC(),
+		reg+int64(86400*c.Len()))
+
+	for ii.HasNext() {
+		p.partitions = append(p.partitions, ii.Next().Begin.Format("2006"))
+	}
+	p.partitions = append(p.partitions, "")
+	return p
+}
---
+++ pkg/charts/entry_test.go
@@ -0,0 +1,94 @@
+package charts
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/nilsbu/lastfm/pkg/rsrc"
+)
+
+func TestChartsFindEntryDates(t *testing.T) {
+	cases := []struct {
+		sums       Charts
+		threshold  float64
+		entryDates []EntryDate
+	}{
+		{
+			Charts{"A": []float64{2, 3, 4}},
+			3,
+			[]EntryDate{{"A", rsrc.ParseDay("2018-01-02")}},
+		},
+		{
+			Charts{
+				"A": []float64{2, 3, 4},
+				"B": []float64{10, 10, 11}},
+			10,
+			[]EntryDate{{"B", rsrc.ParseDay("2018-01-01")}},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			entryDates := c.sums.FindEntryDates(
+				rsrc.ParseDay("2018-01-01"),
+				c.threshold)
+
+			if !reflect.DeepEqual(c.entryDates, entryDates) {
+				t.Errorf("%v != %v", c.entryDates, entryDates)
+			}
+		})
+	}
+}
+
+func TestFilterEntryDates(t *testing.T) {
+	cases := []struct {
+		pre    []EntryDate
+		post   []EntryDate
+		cutoff rsrc.Day
+	}{
+		{
+			[]EntryDate{
+				{"A", rsrc.ParseDay("2018-01-02")},
+				{"B", rsrc.ParseDay("2017-01-02")}},
+			[]EntryDate{{"A", rsrc.ParseDay("2018-01-02")}},
+			rsrc.ParseDay("2018-01-02"),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			post := FilterEntryDates(c.pre, c.cutoff)
+
+			if !reflect.DeepEqual(c.post, post) {
+				t.Errorf("%v != %v", c.post, post)
+			}
+		})
+	}
+}
+
+func TestChartsGetYearPartition(t *testing.T) {
+	cases := []struct {
+		sums      Charts
+		partition Partition
+	}{
+		{
+			Charts{
+				"A": []float64{2, 3, 10},
+				"B": []float64{10, 10, 11}},
+			mapPart{
+				assoc:      map[string]string{"A": "2018", "B": "2017"},
+				partitions: []string{"2017", "2018", ""},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			partition := c.sums.GetYearPartition(rsrc.ParseDay("2017-12-31"), 10)
+
+			if !reflect.DeepEqual(c.partition, partition) { // TODO test partition API
+				t.Errorf("%v != %v", c.partition, partition)
+			}
+		})
+	}
+}
