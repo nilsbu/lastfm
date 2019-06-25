29f65dde35154727c6e0ee745188df3389d6986e
Nils Bussas <nils.bussas@gmail.com>
2019-06-25 19:39:24 +0200

    Add top level help

--- pkg/command/help.go
+++ pkg/command/help.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"github.com/nilsbu/lastfm/pkg/display"
+	"github.com/nilsbu/lastfm/pkg/format"
 	"github.com/nilsbu/lastfm/pkg/store"
 	"github.com/nilsbu/lastfm/pkg/unpack"
 )
@@ -9,6 +10,53 @@ import (
 type help struct{}
 
 func (help) Execute(session *unpack.SessionInfo, s store.Store, d display.Display) error {
-	// TODO fill
+	for _, str := range listCommands() {
+		d.Display(&format.Message{Msg: str})
+	}
+
 	return nil
+}
+
+func listCommands() []string {
+	childStrs := []string{}
+
+	for childName, childNode := range cmdRoot.nodes {
+		if childName != "lastfm" {
+			continue
+		}
+
+		for _, cstr := range listCommandsTree(childNode) {
+			str := childName + " " + cstr
+			childStrs = append(childStrs, str)
+		}
+	}
+
+	return childStrs
+}
+
+func listCommandsTree(node node) []string {
+
+	childStrs := []string{}
+
+	if node.cmd != nil {
+		childStrs = append(childStrs, getCommandDescription(node.cmd))
+	}
+
+	for childName, childNode := range node.nodes {
+		for _, cstr := range listCommandsTree(childNode) {
+			str := childName + " " + cstr
+			childStrs = append(childStrs, str)
+		}
+	}
+
+	return childStrs
+}
+
+func getCommandDescription(cmd *cmd) string {
+	str := ""
+	for _, param := range cmd.params {
+		str += "[" + param.name + ":" + param.kind + "] "
+	}
+
+	return str + "- " + cmd.descr
 }
