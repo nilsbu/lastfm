de61e9c74292de28f3fde14bbfa8256eae24da40
Nils Bussas <nils.bussas@gmail.com>
2020-11-28 19:32:12 +0100

    Optimize normalizers

--- pkg/charts2/normalize.go
+++ pkg/charts2/normalize.go
@@ -1,15 +1,56 @@
 package charts2
 
+import (
+	"runtime"
+)
+
 type normalizer struct {
 	chartsNode
-	totals LazyCharts
+	totals   LazyCharts
+	lineChan chan normalizerJob
+}
+
+type normalizerJob struct {
+	in, out    []float64
+	begin, end int
+	back       chan bool
+}
+
+func newNormalizer(parent LazyCharts, totals LazyCharts) *normalizer {
+	n := &normalizer{
+		chartsNode: chartsNode{parent: parent},
+		totals:     Cache(totals),
+		lineChan:   make(chan normalizerJob),
+	}
+
+	f := func(in, out, totals []float64, begin, end int) {
+		for i, v := range in {
+			if totals[i+begin] > 0 {
+				out[i] = v / totals[i+begin]
+			} else {
+				out[i] = 0
+			}
+		}
+	}
+
+	workers := runtime.NumCPU()
+	for i := 0; i < workers; i++ {
+		go func() {
+			// TODO is there a way to not query the entire length of the totals?
+			totals := n.totals.Row(StringTitle("total"), 0, parent.Len())
+			for job := range n.lineChan {
+				f(job.in, job.out, totals, job.begin, job.end)
+				job.back <- true
+			}
+
+		}()
+	}
+
+	return n
 }
 
 func NormalizeColumn(c LazyCharts) LazyCharts {
-	return &normalizer{
-		chartsNode: chartsNode{parent: c},
-		totals:     Cache(ColumnSum(c)),
-	}
+	return newNormalizer(c, ColumnSum(c))
 }
 
 func NormalizeGaussian(
@@ -17,11 +58,9 @@ func NormalizeGaussian(
 	sigma float64,
 	width int,
 	mirrorBegin, mirrorEnd bool) LazyCharts {
+
 	smooth := Cache(Gaussian(c, sigma, width, mirrorBegin, mirrorEnd))
-	return &normalizer{
-		chartsNode: chartsNode{parent: smooth},
-		totals:     ColumnSum(smooth),
-	}
+	return newNormalizer(smooth, ColumnSum(smooth))
 }
 
 func (c *normalizer) Row(title Title, begin, end int) []float64 {
@@ -29,57 +68,38 @@ func (c *normalizer) Row(title Title, begin, end int) []float64 {
 }
 
 func (c *normalizer) Column(titles []Title, index int) TitleValueMap {
-	total := c.totals.Row(StringTitle("total"), index, index+1)[0]
-
-	col := make(TitleValueMap)
-
-	if total == 0 {
-		for _, title := range titles {
-			col[title.Key()] = TitleValue{
-				Title: title,
-				Value: 0.0,
-			}
-		}
-		return col
-	}
-
-	par := c.parent.Column(titles, index)
-
-	for k, tv := range par {
-		col[k] = TitleValue{
-			Title: tv.Title,
-			Value: tv.Value / total,
+	data := c.Data(titles, index, index+1)
+	tvm := make(TitleValueMap)
+	for title, line := range data {
+		tvm[title] = TitleValue{
+			Title: line.Title,
+			Value: line.Line[0],
 		}
 	}
-	return col
+	return tvm
 }
 
 func (c *normalizer) Data(titles []Title, begin, end int) TitleLineMap {
-	totals := c.totals.Row(StringTitle("total"), begin, end)
-
 	data := make(TitleLineMap)
-	back := make(chan TitleLine)
+	back := make(chan bool, len(titles))
 
-	for k := range titles {
-		go func(k int) {
-			in := c.parent.Row(titles[k], begin, end)
-			out := make([]float64, len(in))
-			for i, v := range in {
-				if totals[i] > 0 {
-					out[i] = v / totals[i]
-				}
-			}
-
-			back <- TitleLine{
-				Title: titles[k],
-				Line:  out,
-			}
-		}(k)
+	for _, title := range titles {
+		out := make([]float64, end-begin)
+		data[title.Key()] = TitleLine{
+			Title: title,
+			Line:  out,
+		}
+		c.lineChan <- normalizerJob{
+			in:    c.parent.Row(title, begin, end),
+			out:   out,
+			begin: begin,
+			end:   end,
+			back:  back,
+		}
 	}
-
 	for range titles {
-		tl := <-back
-		data[tl.Title.Key()] = tl
+		<-back
 	}
+	close(back)
 	return data
 }
