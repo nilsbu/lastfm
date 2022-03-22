25c715e877d40a65d85e6747183460e594eed943
Nils Bussas <nils.bussas@gmail.com>
2022-03-22 15:51:53 +0100

    Add graph

---
+++ pkg/pipeline/graph.go
@@ -0,0 +1,129 @@
+package pipeline
+
+import (
+	"github.com/nilsbu/lastfm/pkg/charts"
+)
+
+type graph struct {
+	root    *node
+	counter int
+	caches  [][]string
+	limit   int
+}
+
+type node struct {
+	children   map[string]*node
+	charts     charts.Charts
+	lastAccess int
+}
+
+func newGraph(limit int) *graph {
+	return &graph{
+		root:   &node{children: map[string]*node{}},
+		caches: [][]string{},
+		limit:  limit,
+	}
+}
+
+func (c *graph) get(steps []string) charts.Charts {
+	c.counter++
+	n := c.find(steps, c.counter)
+	if n != nil {
+		return n.charts
+	} else {
+		return nil
+	}
+}
+
+func (c *graph) set(steps []string, charts charts.Charts) charts.Charts {
+	c.counter++
+	n := c.find(steps[:len(steps)-1], c.counter)
+	if n != nil {
+		for i := 0; i < len(c.caches); {
+			if isPredecessor(steps, c.caches[i]) && len(steps) < len(c.caches[i]) {
+				c.removeCache(i)
+			} else {
+				i++
+			}
+		}
+		n.children[steps[len(steps)-1]] = &node{
+			map[string]*node{},
+			charts,
+			c.counter,
+		}
+
+		if len(steps) == 1 || steps[len(steps)-1] == "cache" {
+			c.caches = append(c.caches, steps)
+			c.prune()
+		}
+
+		return charts
+	} else {
+		return nil
+	}
+}
+
+func (c *graph) find(steps []string, counter int) *node {
+	n := c.root
+	if counter > 0 {
+		n.lastAccess = counter
+	}
+	for _, step := range steps {
+		if next, ok := n.children[step]; ok {
+			n = next
+			if counter > 0 {
+				n.lastAccess = counter
+			}
+		} else {
+			return nil
+		}
+	}
+	return n
+}
+
+func (c *graph) prune() {
+	for len(c.caches) > c.limit {
+		oldestAge, oldestId := -1, -1
+		for i, cache := range c.caches {
+			if isPredecessor(cache, c.caches[len(c.caches)-1]) {
+				continue
+			}
+			age := c.find(cache, -1).lastAccess
+			if oldestAge < age ||
+				(oldestAge == age && len(cache) > len(c.caches[oldestId])) {
+				oldestAge = age
+				oldestId = i
+			}
+		}
+		if oldestId != -1 {
+			c.removeCache(oldestId)
+		} else {
+			break
+		}
+	}
+}
+
+func (c *graph) removeCache(cacheId int) {
+	// remove from nodes (includes children)
+	steps := c.caches[cacheId]
+	parent := c.find(steps[:len(steps)-1], -1)
+	delete(parent.children, steps[len(steps)-1])
+
+	// remove from caches list
+	newList := make([][]string, 0)
+	newList = append(newList, c.caches[:cacheId]...)
+	newList = append(newList, c.caches[cacheId+1:]...)
+	c.caches = newList
+}
+
+func isPredecessor(steps, comp []string) bool {
+	if len(steps) > len(comp) {
+		return false
+	}
+	for i, step := range steps {
+		if step != comp[i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ pkg/pipeline/graph_test.go
@@ -0,0 +1,234 @@
+package pipeline
+
+import (
+	"testing"
+
+	"github.com/nilsbu/lastfm/pkg/charts"
+)
+
+func TestCache(t *testing.T) {
+	// the semantics of the charts are irrelevant for this test,
+	// all that matters is that pointers are recognizable.
+	cs := make([]charts.Charts, 100)
+	for i := range cs {
+		cs[i] = charts.FromMap(map[string][]float64{})
+	}
+
+	type setget struct {
+		method string
+		steps  []string
+		charts charts.Charts
+	}
+
+	for _, c := range []struct {
+		name  string
+		limit int
+		cmds  []setget
+	}{
+		{
+			"no steps",
+			1,
+			[]setget{},
+		},
+		{
+			"get without set",
+			1,
+			[]setget{{
+				"get",
+				[]string{"sum"},
+				nil,
+			}},
+		},
+		{
+			"set 1st step",
+			1,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}},
+		},
+		{
+			"read 1st step",
+			1,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"get",
+				[]string{"sum"},
+				cs[0],
+			}},
+		},
+		{
+			"2 steps",
+			1,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"set",
+				[]string{"sum", "id"},
+				cs[1],
+			}, {
+				"get",
+				[]string{"sum", "id"},
+				cs[1],
+			}},
+		},
+		{
+			"2 roots",
+			2,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"set",
+				[]string{"id"},
+				cs[1],
+			}, {
+				"get",
+				[]string{"id"},
+				cs[1],
+			}, {
+				"get",
+				[]string{"sum"},
+				cs[0],
+			}},
+		},
+		{
+			"1st root pruned by cache",
+			2,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"set",
+				[]string{"id"},
+				cs[1],
+			}, {
+				"set",
+				[]string{"id", "cache"},
+				cs[2],
+			}, {
+				"get",
+				[]string{"sum"},
+				nil,
+			}, {
+				"get",
+				[]string{"id", "cache"},
+				cs[2],
+			}},
+		},
+		{
+			"prune children first",
+			3,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"set",
+				[]string{"sum", "cache"},
+				cs[3],
+			}, {
+				"set",
+				[]string{"id"},
+				cs[1],
+			}, {
+				"set",
+				[]string{"id", "cache"},
+				cs[2],
+			}, {
+				"get",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"get",
+				[]string{"sum", "cache"},
+				nil,
+			}},
+		},
+		{
+			"overwrite can be done",
+			3,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"set",
+				[]string{"sum"},
+				cs[1],
+			}, {
+				"get",
+				[]string{"sum"},
+				cs[1],
+			}},
+		},
+		{
+			"overwrite keeps cache list intact",
+			3,
+			[]setget{{
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"set",
+				[]string{"sum", "cache"},
+				cs[3],
+			}, {
+				"set",
+				[]string{"sum"},
+				cs[0],
+			}, {
+				"set",
+				[]string{"id"},
+				cs[1],
+			}, {
+				"set",
+				[]string{"id", "cache"},
+				cs[2],
+			}, {
+				"get",
+				[]string{"sum", "cache"},
+				nil,
+			}},
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			cache := newGraph(c.limit)
+
+			for i, cmd := range c.cmds {
+				var result charts.Charts
+				switch cmd.method {
+				case "get":
+					result = cache.get(cmd.steps)
+
+				case "set":
+					result = cache.set(cmd.steps, cmd.charts)
+
+				default:
+					t.Fatalf("method '%v' not supported", cmd.method)
+				}
+
+				if cmd.charts != result {
+					if cmd.charts == nil {
+						t.Fatalf("expected result of step %v (%v %v) to be nil but wasn't",
+							i, cmd.method, cmd.steps)
+					} else if result == nil {
+						t.Fatalf("expected result of step %v (%v %v) to not be nil but wasn't",
+							i, cmd.method, cmd.steps)
+					} else {
+						t.Fatalf("result of step %v (%v %v) is wrong but not nil",
+							i, cmd.method, cmd.steps)
+					}
+				}
+			}
+		})
+	}
+}
