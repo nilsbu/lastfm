f2435344841fcca0399c7d24d2fbfe50fbf5602f
Nils Bussas <nils.bussas@gmail.com>
2023-04-22 20:47:53 +0000

    Limit buttons per page

--- react/lastfm/src/Menu.tsx
+++ react/lastfm/src/Menu.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import type { ButtonGroup } from './menus';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const BUTTONS_PER_PAGE = 5;
+
 interface MenuProps {
   onMethodChange: (newMethod: string) => void;
   buttons: ButtonGroup;
@@ -9,23 +11,56 @@ interface MenuProps {
 
 function Menu(props: MenuProps) {
   const [active, setActive] = useState(props.buttons.default);
+  const [startIndex, setStartIndex] = useState(0);
 
-  const handleClick = (name : string) => {
+  const handleButtonClick = (name: string) => {
     setActive(name);
     props.onMethodChange(name);
   };
+
+  const handlePrevClick = () => {
+    const newStartIndex = Math.max(0, startIndex - BUTTONS_PER_PAGE);
+    setStartIndex(newStartIndex);
+  };
+
+  const handleNextClick = () => {
+    const newStartIndex = Math.min(
+      props.buttons.buttons.length - BUTTONS_PER_PAGE,
+      startIndex + BUTTONS_PER_PAGE
+    );
+    setStartIndex(newStartIndex);
+  };
+
+  const visibleButtons = props.buttons.buttons.slice(
+    startIndex,
+    startIndex + BUTTONS_PER_PAGE
+  );
 
   return (
     <div className="bg-secondary p-3">
       <div className="row">
         <div className="col-auto">
-          {props.buttons.buttons.map(button => (
+          {startIndex > 0 && (
+            <button className="btn btn-light mx-1" onClick={handlePrevClick}>
+              &lt;
+            </button>
+          )}
+          {visibleButtons.map((button) => (
             <button
               key={button.function}
-              onClick={() => handleClick(button.function)}
-              className={`btn btn-light mx-1 ${active === button.function ? 'active' : ''}`}
-            >{button.name}</button>
+              onClick={() => handleButtonClick(button.function)}
+              className={`btn btn-light mx-1 ${
+                active === button.function ? 'active' : ''
+              }`}
+            >
+              {button.name}
+            </button>
           ))}
+          {startIndex + BUTTONS_PER_PAGE < props.buttons.buttons.length && (
+            <button className="btn btn-light mx-1" onClick={handleNextClick}>
+              &gt;
+            </button>
+          )}
         </div>
       </div>
     </div>
--- react/lastfm/src/menus.tsx
+++ react/lastfm/src/menus.tsx
@@ -13,13 +13,33 @@ export const buttons: MenuDefinition = {
   'topLevel': {buttons: [
     { function: 'total', name: 'Total' },
     { function: 'fade', name: 'Fade' },
+    { function: 'period', name: 'Period' },
   ], default: 'total'},
   'fade': {buttons:[
     { function: '30', name: '30' },
     { function: '365', name: '365' },
     { function: '1000', name: '1000' },
     { function: '3653', name: '3653' },
-  ], default: '365'}
+  ], default: '365'},
+  'period': {buttons:[
+    { function: '2007', name: '2007' },
+    { function: '2008', name: '2008' },
+    { function: '2009', name: '2009' },
+    { function: '2010', name: '2010' },
+    { function: '2011', name: '2011' },
+    { function: '2012', name: '2012' },
+    { function: '2013', name: '2013' },
+    { function: '2014', name: '2014' },
+    { function: '2015', name: '2015' },
+    { function: '2016', name: '2016' },
+    { function: '2017', name: '2017' },
+    { function: '2018', name: '2018' },
+    { function: '2019', name: '2019' },
+    { function: '2020', name: '2020' },
+    { function: '2021', name: '2021' },
+    { function: '2022', name: '2022' },
+    { function: '2023', name: '2023' },
+  ], default: '2023'}
 };
   
   export const getMenus = (topLevelFunction : string) => {
@@ -28,6 +48,8 @@ export const buttons: MenuDefinition = {
         return ['topLevel'];
       case 'fade':
         return ['topLevel', 'fade'];
+      case 'period':
+        return ['topLevel', 'period'];
       default:
         return ['topLevel'];
     }
